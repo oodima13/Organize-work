38f5da498b6f794a275e9f5963e1d83c4e89a2e6
Dmitry <oodima13@gmail.com>
2021-12-16 00:17:07 +0100

    first commit
    Connecting with database ( php my admin)

---
+++ delete.php
@@ -0,0 +1,10 @@
+<?php
+require 'configDB.php';
+$id = $_GET['id'];
+
+$sql = 'DELETE FROM `tasks` WHERE `id` = ?';
+$query = $pdo->prepare($sql);
+$query->execute([$id]);
+
+header('Location: /');
+?>
